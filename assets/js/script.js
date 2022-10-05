12a21d0167258e46415619981fa9e010c8244b41
Mia <113086524+MiaYun98@users.noreply.github.com>
2022-10-05 15:07:01 -0700

    done

--- assets/js/script.js
+++ assets/js/script.js
@@ -55,40 +55,48 @@ function renderdata() {
 renderdata();
 
 $("#0").on("click", function(event) {
-    console.log("click")
     saveData.splice(0, 1, timeset[0].value)
+    $(".saved").text("9AM saved!");
     storedata();
 })
 $("#1").on("click", function(event) {
     saveData.splice(1, 1, timeset[1].value)
+    $(".saved").text("10AM saved!");
     storedata();
 })
 $("#2").on("click", function(event) {
     saveData.splice(2, 1, timeset[2].value)
+    $(".saved").text("11AM saved!");
     storedata();
 })
 $("#3").on("click", function(event) {
     saveData.splice(3, 1, timeset[3].value)
+    $(".saved").text("12PM saved!");
     storedata();
 })
 $("#4").on("click", function(event) {
     saveData.splice(4, 1, timeset[4].value)
+    $(".saved").text("1PM saved!");
     storedata();
 })
 $("#5").on("click", function(event) {
     saveData.splice(5, 1, timeset[5].value)
+    $(".saved").text("3PM saved!");
     storedata();
 })
 $("#6").on("click", function(event) {
     saveData.splice(6, 1, timeset[6].value)
+    $(".saved").text("4PM saved!");
     storedata();
 })
 $("#7").on("click", function(event) {
     saveData.splice(7, 1, timeset[7].value)
+    $(".saved").text("5PM saved!");
     storedata();
 })
 $("#8").on("click", function(event) {
     saveData.splice(8, 1, timeset[8].value)
+    $(".saved").text("6PM saved!");
     storedata();
 })
 
