17fc81f9d33b84394f30c98b03df6e09916fc5b3
Mia <113086524+MiaYun98@users.noreply.github.com>
2022-10-09 16:56:22 -0700

    10/9

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,60 +4,76 @@ $("#currentDay").text(today.format("dddd MMM Do, YYYY"));
 
 // variable set for the current time with the 24 hour system
 var current = moment().format("HH"); 
-
+console.log(current)
+console.log(typeof current)
 // var for the array of the time row
 var timeset = $("textarea");
+console.log(timeset)
+timeset.each(function () {
 
-for (var i = 0; i < timeset.length; i++) {
-        //past background
-    if (current > timeset[i].dataset.state) {
-        timeset[i].classList.add("past");
-        timeset[i].classList.remove("present");
-        timeset[i].classList.remove("future")
-    } else if (current == timeset[i].dataset.state) {
+    if (current > parseInt($(this).attr("data-state"))) {
+        $(this).addClass("past");
+    } else if (current == parseInt($(this).attr("data-state"))) {
         //current background
-        timeset[i].classList.add("present");
-        timeset[i].classList.remove("past");
-        timeset[i].classList.remove("future")
-    } else if (current < timeset[i].dataset.state) {
+        $(this).addClass("present");
+    } else if (current < parseInt($(this).attr("data-state"))) {
         // future background
-        timeset[i].classList.add("future");
-        timeset[i].classList.remove("present");
-        timeset[i].classList.remove("past")
+        $(this).addClass("future");
     }
-};
+});
 
-if (current > 9) {
-    timeset[0].classList.remove("future")
-    timeset[0].classList.add("past");
-}
-
-var saveData = ['','','','','','','','',''];
+var saveData = ['0','1','2','3','4','5','6','7','8'];
+var savedsave = [];
 
 function storedata() {
     localStorage.setItem("saveData",JSON.stringify(saveData));
 }
 
+storedata(); 
+saveData[0] = 'text'
+storedata();
 function renderdata() {
     saveData = JSON.parse(localStorage.getItem("saveData"));
-    timeset[0].value = saveData[0]
-    timeset[1].value = saveData[1]
-    timeset[2].value = saveData[2]
-    timeset[3].value = saveData[3]
-    timeset[4].value = saveData[4]
-    timeset[5].value = saveData[5]
-    timeset[6].value = saveData[6]
-    timeset[7].value = saveData[7]
-    timeset[8].value = saveData[8]
+    if (saveData !== null) {
+        savedsave = saveData;
+    }
+    console.log(savedsave)
+    // if (saveData === null) {
+    //     saveData[0] = '';
+    //     saveData[1] = '';
+    //     saveData[2] = '';
+    //     saveData[3] = '';
+    //     saveData[4] = '';
+    //     saveData[5] = '';
+    //     saveData[6] = '';
+    //     saveData[7] = '';
+    //     saveData[8] = '';
+    // } else {
+    //     timeset[0].value = saveData[0]
+    //     timeset[1].value = saveData[1]
+    //     timeset[2].value = saveData[2]
+    //     timeset[3].value = saveData[3]
+    //     timeset[4].value = saveData[4]
+    //     timeset[5].value = saveData[5]
+    //     timeset[6].value = saveData[6]
+    //     timeset[7].value = saveData[7]
+    //     timeset[8].value = saveData[8]
+    // }
+    console.log(saveData);
+
 }
 
 //button click!! 
 renderdata();
 
 $("#0").on("click", function(event) {
-    saveData.splice(0, 1, timeset[0].value)
-    $(".saved").text("9AM saved!");
-    storedata();
+    // saveData.splice(0, 1, timeset[0].value)
+    // saveData[0] = timeset[0].value;
+    // const text = $(this).siblings("textarea").val()
+    // saveData[0] = text;
+    // console.log(saveData[0])
+    // $(".saved").text("9AM saved!");
+    // storedata();
 })
 $("#1").on("click", function(event) {
     saveData.splice(1, 1, timeset[1].value)
